af3f52b438afea0a13d6de6bcbc80921aed369fa
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2022-05-31 23:33:22 +0300

    added eslint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from "react-router-dom";
-import {withErrorBoundary} from 'react-error-boundary';
+import React, { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { withErrorBoundary } from 'react-error-boundary';
 
-const App = () => {
-    return (
-        <div className="App">
-            <h1>Главная</h1>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/about" element={<div>About</div>}/>
-                    <Route path="/registration" element={<div>Registration</div>}/>
-                    <Route path="login" element={<div>Login</div>}/>
-                    <Route path="/game" element={<div>Game</div>}/>
-                    <Route exact path="/error" element={<div>Error</div>}/>
-                    <Route exact path="/" element={<div>Home</div>}/>
-                    <Route path="*" element={<div>NotFound</div>}/>
-                </Routes>
-            </Suspense>
-        </div>
-    );
-};
+const About = lazy(() => import('./pages/About/About.tsx'))
+
+const App = () => (
+  <div className="App">
+    <h1>Главная</h1>
+
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/registration" element={<div>Registration</div>} />
+        <Route path="login" element={<div>Login</div>} />
+        <Route path="/game" element={<div>Game</div>} />
+        <Route exact path="/error" element={<div>Error</div>} />
+        <Route exact path="/" element={<div>Home</div>} />
+        <Route path="*" element={<div>NotFound</div>} />
+      </Routes>
+    </Suspense>
+  </div>
+)
 
 export default withErrorBoundary(App, {
-        fallback: <>Что-то пошло не так.</>
-    }
-);
-
+  fallback: <>Что-то пошло не так.</>,
+})
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const About:React.FC = () => <div className="aa">About</div>
+
+export default About
