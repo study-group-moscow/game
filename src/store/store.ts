ce3a5f192713e0fb5e59f0d94ad133c74a182da3
Maksim Yakovlev <MViYakovlev@sberbank.ru>
2022-05-31 10:41:35 +0300

    Add redux

---
+++ src/store/store.ts
@@ -0,0 +1,25 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import userReducer from './reducers/UserSlice'
+import {userAPI} from "../services/UserService";
+
+const rootReducer = combineReducers({
+    userReducer,
+    [userAPI.reducerPath]: userAPI.reducer
+})
+
+
+export const createStore = () => {
+    return configureStore({
+        reducer: rootReducer,
+
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({
+                serializableCheck: false
+            })
+                .concat(userAPI.middleware)
+    })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof createStore>
+export type AppDispatch = AppStore['dispatch']
