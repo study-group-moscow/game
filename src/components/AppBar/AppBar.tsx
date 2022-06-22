1bd17c568303733617d4ef1927408dfa1283788e
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2022-06-22 04:01:09 +0300

    init

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,68 +1,14 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
+import Drawer from './Drawer'
 
-import Avatar from '@mui/material/Avatar';
+const CustomAppBar = () => (
+  <AppBar position='static'>
+    <Toolbar>
+      <Drawer />
+    </Toolbar>
+  </AppBar>
+)
 
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-
-const settings = ['Профиль', 'Выход'];
-
-const CustomAppBar = () => {
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  return (
-    <AppBar position='static'>
-      <Toolbar>
-        <AdbIcon sx={{ mr: 'auto' }} />
-
-        <Box>
-          <Tooltip title='Open settings'>
-            <IconButton onClick={handleOpenUserMenu} sx={{ px: 0 }}>
-              <Avatar sx={{ bgcolor: 'transparent' }} src='' />
-            </IconButton>
-          </Tooltip>
-
-          <Menu
-            sx={{ mt: '55px' }}
-            id='menu-appbar'
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right'
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right'
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            {settings.map((setting) => (
-              <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                <Typography textAlign='center'>{setting}</Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
-};
 export default CustomAppBar
-
---
+++ src/components/AppBar/Drawer.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import IconButton from '@mui/material/IconButton';
+import SailingIcon from '@mui/icons-material/Sailing';
+
+export default () => {
+  const [isOpened, setIsOpened] = React.useState(false)
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (
+      event.type === 'keydown'
+      && ((event as React.KeyboardEvent).key === 'Tab'
+      || (event as React.KeyboardEvent).key === 'Shift')
+    ) { return; }
+
+    setIsOpened(open)
+  }
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role='presentation'
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+      <Divider />
+
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  )
+
+  return (
+    <>
+      <IconButton color='inherit' onClick={toggleDrawer(true)}>
+        <SailingIcon sx={{ mr: 'auto' }} />
+      </IconButton>
+
+      <Drawer anchor='left' open={isOpened} onClose={toggleDrawer(false)}>
+        {list()}
+      </Drawer>
+    </>
+  )
+}
