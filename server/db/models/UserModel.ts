1767c5afdefdf1ae73a26fcbe82ab776fcee0082
Maksim Yakovlev <MViYakovlev@sberbank.ru>
2022-08-02 17:21:50 +0300

    add sequelize

---
+++ server/db/models/UserModel.ts
@@ -0,0 +1,26 @@
+import {db} from '../db';
+import {DataTypes} from "sequelize";
+
+export const UserModel = db.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    first_name: {
+        type: DataTypes.STRING,
+    },
+    second_name: {
+        type: DataTypes.STRING,
+    },
+    display_name: {
+        type: DataTypes.STRING,
+    },
+    theme: {
+        type: DataTypes.STRING,
+    }
+},{
+    tableName: 'user',
+    timestamps: false
+});
