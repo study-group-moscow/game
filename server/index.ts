acad76f9aa228a9743e2e6f79d9b52cfe73de82e
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2022-08-04 18:41:42 +0300

    sever fix

--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,27 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import userRouter from './routes/user.routes';
 import postRouter from './routes/post.routes';
+
 const PORT = process.env.PORT || 8989;
 
 const app = express();
 
-const whitelist = [`http://localhost:5000`];
 const corsOptions = {
-  origin: function (origin: string, callback: (obj: Error | null, bool?: boolean) => void) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true,
+  origin: ['http://localhost:5000'],
+  credentials: true
 }
-// @ts-ignore
+
 app.use(cors(corsOptions))
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello');
+  res.send('hello');
 });
 
 app.use('/user', userRouter);
 app.use('/post', postRouter);
 
 app.listen(PORT, () => {
-    console.log(`app run on port ${PORT}`);
+  console.log(`app run on port ${PORT}`);
 });
