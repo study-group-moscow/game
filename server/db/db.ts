52b0dd7e4a588d534dd8f8e41eb4b9ddb05a65d3
MaksimYakovlev <mvyakovlev@outlook.com>
2022-08-06 21:46:22 +0300

    add logger and env

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,12 +1,21 @@
 import { Sequelize } from 'sequelize';
 
+declare let process : {
+  env: {
+    DB_PORT: number,
+    DB_HOST: string,
+    DB_PASSWORD: string,
+    DB_USER_NAME: string,
+    DB: string
+  }
+}
 export const db = new Sequelize(
-  'postgres',
-  'postgres',
-  'postgres',
+  process.env.DB,
+  process.env.DB_USER_NAME,
+  process.env.DB_PASSWORD,
   {
     dialect: 'postgres',
-    host: 'localhost',
-    port: 7777
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
   }
 )
